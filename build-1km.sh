92d03e3c04a3d52a88768cd3253a0eeeb84d369d
Alex Bertram <alex@bedatadriven.com>
2020-12-10 09:04:24 +0100

    Updated 1km script to cache download

--- build-1km.sh
+++ build-1km.sh
@@ -6,7 +6,9 @@ if [ ! -f tif/ppp_${YEAR}_1km_Aggregated.tif ]; then
     wget -q ftp://ftp.worldpop.org/GIS/Population/Global_2000_2020/${YEAR}/0_Mosaicked/ppp_${YEAR}_1km_Aggregated.tif -P tif
 fi
 
-gdalwarp -t_srs EPSG:3857 tif/ppp_${YEAR}_1km_Aggregated.tif tif/ppp_${YEAR}_1km_Aggregated_mercator.tif
+if [ ! -f tif/ppp_${YEAR}_1km_Aggregated_mercator.tif]; then
+    gdalwarp -t_srs EPSG:3857 tif/ppp_${YEAR}_1km_Aggregated.tif tif/ppp_${YEAR}_1km_Aggregated_mercator.tif
+fi
 
 gdaldem color-relief tif/ppp_${YEAR}_1km_Aggregated_mercator.tif colors.cpt tif/ppp_${YEAR}_1km_Aggregated_colors.tif
 
